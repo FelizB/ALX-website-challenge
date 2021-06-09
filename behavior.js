1d27d387864c64325aa2b154e6ecb517423bfc21
Felix <bosirefelix97@gmail.com>
2021-06-09 14:01:42 +0300

    the final webpage

---
+++ behavior.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+     
+var modal = document.getElementById("myModal");
+
+var img2= document.getElementById("smart_thumbnail2")
+var img = document.getElementById("smart_thumbnail");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+if (true) {
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+}
+
+if (true) {
+	img2.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+}
+
+
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
+
+});
